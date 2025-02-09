b2d16ac66781b8ffa9c5084fb97b346f9bbd9d67
Martin Babak <martinyis11@gmail.com>
2025-02-08 19:28:33 -0500

    Changeed spelling

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "UFA - Ultra Footware Accelerator",
+  title: "UFA - Ultra Footwear Accelerator",
   description:
     "Founded in 2016 and based in Waltham, Massachusetts, Ultra Footwear Accelerator is dedicated to helping brands bring innovative footwear and accessories to life. We collaborate with a wide network of global suppliers, offering expertise in materials such as leathers, recycled leathers, advanced outsoles, insoles, and high-performance components. In addition to footwear, we support the development of premium accessories, including bags and other complementary products. Our mission is to empower brands in creating cutting-edge solutions. For inquiries, please reach out to us through our contact form.",
   icons: {
